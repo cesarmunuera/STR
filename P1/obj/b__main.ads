d2516547a85be6d30b172b249401ce6d47a3a090
cesar <cesar.munuera@gmail.com>
2022-02-16 13:52:38 +0100

    p1 a la mitad

--- P1/obj/b__main.ads
+++ P1/obj/b__main.ads
@@ -40,7 +40,7 @@ package ada_main is
    pragma Export (C, main, "main");
 
    type Version_32 is mod 2 ** 32;
-   u00001 : constant Version_32 := 16#497b956c#;
+   u00001 : constant Version_32 := 16#8e5b7441#;
    pragma Export (C, u00001, "mainB");
    u00002 : constant Version_32 := 16#2e11c0b1#;
    pragma Export (C, u00002, "system__standard_libraryB");
--- P1/src/main.adb
+++ P1/src/main.adb
@@ -3,7 +3,7 @@ use ada.Text_IO;
 
 procedure main is
    type dia_t is range 0..31;
-   type mes_t is (enero , febrero , marzo , abril , mayo , junio, julio, agosto, septiembre, octubre, noviembre, diciembre );
+   type mes_t is (enero , febrero , marzo , abril , mayo , junio, julio, agosto, septiembre, octubre, noviembre, diciembre);
    type anio_t is range 1900..2100;
    type temp_t is delta 0.01 range -25.0..75.0;
 
@@ -11,6 +11,7 @@ procedure main is
    package ES_mes is new Ada.Text_Io.Enumeration_Io(mes_t);
    package ES_anio is new Ada.Text_Io.Integer_Io(anio_t);
    package ES_temp is new Ada.Text_Io.Fixed_Io(temp_t);
+   package Es_IO is new Ada.Text_IO.Integer_IO(Natural);
 
    type fecha_r is record
       Dia : dia_t;
@@ -24,36 +25,47 @@ procedure main is
    end record ;
    reg_r : registro_t;
 
-   type vector_t is array (1..2) of registro_t;
-   vector : vector_t;
+   type array_t is array (integer range <>) of registro_t;
+   type pArray_t is access array_t;
+   v : pArray_t;
+   n : Integer;
+
+   --type vector_t is array (1..2) of registro_t;
+   --vector : vector_t;
 
 
 begin
-   for i in 1..2 loop
-      Put("Primero, introduciremos la fecha ... ");
-      New_Line(1);
-      Put("Introduce el dia:");
+
+   Put("Antes de nada, debe indicar cuantas mediciones quiere introducir: ");
+   Es_IO.Get(n);
+   New_Line(1);
+   v := new array_t (1..n);
+
+   for i in 1..n loop
+      Put_Line("Primero, introduciremos la fecha ... ");
+      Put("Introduce el dia. Del 1 al 31: ");
       ES_dia.Get(reg_r.Fecha.Dia);
       New_Line(1);
 
-      Put("Introduce el mes:");
+      Put("Introduce el mes. De enero a diciembre: ");
       ES_mes.Get(reg_r.Fecha.Mes);
       New_Line(1);
 
-      Put("Introduce el año:");
+      Put("Introduce el año. Del 1900 al 2100: ");
       ES_anio.Get(reg_r.Fecha.Anio);
       New_Line(1);
 
-      Put("Ahora, introduce la temperatura de ese dia: ");
+      Put("Ahora, introduce la temperatura de ese dia. Desde -25.00 a 75.00 grados: ");
       ES_temp.Get(reg_r.Temperatura);
       New_Line(1);
 
       --Ahora guardamos en el array
-      vector(i) := reg_r;
+      v(i) := reg_r;
+
    end loop ;
 
-   Put_Line("Los valores introducidos son:");
-   Put_Line("El dia " & vector(1).Fecha.Dia'Image & ", mes " & vector(1).Fecha.Mes'Image & ", año " & vector(1).Fecha.Anio'Image & ", habia una temperatura de " & vector(1).Temperatura'Image);
+   --Put_Line("Los valores introducidos son:");
+   --Put_Line("El dia " & v(1).Fecha.Dia'Image & ", mes " & v(1).Fecha.Mes'Image & ", año " & v(1).Fecha.Anio'Image & ", habia una temperatura de " & v(1).Temperatura'Image);
 
 exception
    when Data_Error =>
