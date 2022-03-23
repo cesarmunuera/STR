0778a4be1c602bc5a3a1e4c9592a6bdf738b2e51
cesar <cesar.munuera@gmail.com>
2022-03-23 14:03:23 +0100

    medir 1 revisar

--- P4/src/medir1.adb
+++ P4/src/medir1.adb
@@ -2,7 +2,8 @@ with Calefactor, Sensor, Ada.Real_Time;
 use Calefactor, Sensor, Ada.Real_Time;
 
 procedure Medir1 is
-   temp_e, temp_actual : Temperaturas;
+   potencia : Potencias;
+   temp_e, temp_actual, temp_aux, Cp : Temperaturas;
    tiempo_inicial, tiempo_final : Time;
    l_dif_tiempo : Time_Span;
 begin
@@ -11,25 +12,35 @@ begin
 
 
    --Encendemos el horno
-   Escribir(1000);
+   potencia := 1000;
+   Escribir(potencia);
 
 
-   --Calculamos L
+   --Calculamos L. Mientras que la diferencia de la temperatura ambiente con la del interior no sea notable,
+   --seguimos esperando a que el horno siga calentando
    Leer(temp_actual);
    tiempo_inicial := Clock;
    while not (temp_e - temp_actual > 0.1) loop
       --Esperamos a que arranque
-      Leer(temp_actual)
+      Leer(temp_actual):
    end loop;
    tiempo_final := Clock;
-   --Sacamos el tiempo inicial y luego el tiempo que tarda en variar la temperatura
-   --La diferencia, es el tiempo L
+   --Sacamos el tiempo inicial y luego el tiempo que tarda en variar la temperatura. Esto es el tiempo L
    l_dif_tiempo := tiempo_inicial - tiempo_final;
 
 
-   --Calculamos Cp
-
-
+   --Calculamos Cp. Tenemos que esperar a que la diferencia de temperatura en un instante sea minima con
+   --el instante anterior, que es cuando la curva se ha estabilizado
+   Leer(temp_actual);
+   Leer(temp_aux);
+   while not (temp_actual - temp_aux < 0.1) loop
+      --Esperamos a que llegue al regimen permanente
+      --Acualizamos variables
+      temp_aux := temp_actual;
+      Leer(temp_actual);
+   end loop;
+   --Calculamos con 0 = P - Cp(T - Te). Cp = (P / T-Te). P = potencia, T = temp_actual, Te = temp_e.
+   Cp = (potencia / (temp_actual - temp_e));
 
 
 
