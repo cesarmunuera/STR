46515c2f2d70c2987d2f796d2ae2321b082a71f1
cesar <cesar.munuera@gmail.com>
2022-03-30 23:24:37 +0200

    pequeños ajustes

--- P4/src/medir1.adb
+++ P4/src/medir1.adb
@@ -8,14 +8,16 @@ procedure medir1 is
    l_dif_tiempo : Time_Span;
    Cp : Float;
 begin
-   --Calculamos Te
+   --Calculamos Te.
    Leer(temp_e);
+   Put_Line("La temperatura ambiente es: " & temp_e'Image);
 
 
 
-   --Encendemos el horno
+   --Encendemos el horno.
    potencia := 1000;
    Escribir(potencia);
+   Put_Line("La potencia es: " & potencia'Image);
 
 
 
@@ -29,17 +31,17 @@ begin
    end loop;
    tiempo_final := Clock;
    temp_despegue := temp_actual;
-   --Sacamos el tiempo inicial y luego el tiempo que tarda en variar la temperatura. Esto es el tiempo L
+   --Sacamos el tiempo inicial y luego el tiempo que tarda en variar la temperatura. Esto es el tiempo L.
    l_dif_tiempo := tiempo_inicial - tiempo_final;
 
 
 
    --Calculamos Cp. Tenemos que esperar a que la diferencia de temperatura en un instante sea minima con
    --el instante anterior, que es cuando la curva se ha estabilizado. La diferencia decrece.
-   --Si volvemos a leer temp_aux, la diferencia en de ambos es 0 y no entramos en el while
+   --Si volvemos a leer temp_aux, la diferencia en de ambos es 0 y no entramos en el while.
    temp_aux := temp_actual;
    Leer(temp_actual);
-   --Este bucle es el tramo del regimen transitorio
+   --Este bucle es el tramo del regimen transitorio.
    while (temp_actual - temp_aux < 0.1) loop
       --Esperamos a que llegue al regimen permanente, actualizando variables.
       temp_aux := temp_actual;
@@ -47,10 +49,11 @@ begin
    end loop;
    --Calculamos con 0 = P - Cp(T - Te). Cp = (P / T-Te). P = potencia, T = temp_actual, Te = temp_e.
    Cp = (potencia / (temp_actual - temp_e));
+   Put_Line("Cp es: " & Cp'Image);
 
 
 
-   --Apagamos el horno
+   --Apagamos el horno.
    potencia := 0.0;
    Escribir(potencia);
 
--- P4/src/medir2.adb
+++ P4/src/medir2.adb
@@ -2,7 +2,7 @@ with Calefactor, Sensor, Ada.Real_Time, Ada.Text_IO;
 use Calefactor, Sensor, Ada.Real_Time, Ada.Text_IO;
 
 procedure medir2 is
-   temp_actual, temp_anterior, temp_aux, temp_despegue, dif_temp, temp_media, temp_ambiente : Temperaturas;
+   temp_actual, temp_anterior, temp_aux, temp_despegue, dif_temp, temp_media, Te : Temperaturas;
    potencia : Potencias;
    Cp, pendiente_actual, pendiente_anterior, Ct : Float;
    fin : Boolean;
@@ -11,22 +11,39 @@ procedure medir2 is
 begin
 
    fin := false;
-   temp_despegue := 4;
-   temp_ambiente :=2;
 
-   --Encendemos el horno
+   --Valores obtenidos en medir1.
+   Cp := 4;
+   temp_despegue := 4;
+   Te :=2;
+
+
+
+   --Encendemos el horno.
    potencia := 1000;
    Escribir(potencia);
 
-   --Esperamos a que tengamos temperatura distinta de ambiente, y tengamos pendiente
+
+
+   --Partimos de la temperatura en la que se supone que entramos en el regimen transitorio.
+   --Por tanto, esto simplemente es un checkeo, ya que la actual tiene que ser superior.
+   --Tomamos la temperatura ya que este valor será el del T1 para sacar la pendiente despues.
    Leer(temp_actual);
+   tiempo_anterior := Clock;
    while (temp_actual < temp_despegue) loop
       Put_Line("Esperando a llegar al regimen transitorio");
       Leer(temp_actual);
       tiempo_anterior := Clock;
    end loop;
 
-   --Calculamos el punto con mayor pendiente
+
+
+   --Calculamos el punto con mayor pendiente.
+   --Antes hemos sacado el tiempo de esta temperatura, y ahora lo asignamos al anterior, por
+   --eso el tiempo lo hemos tomado en su momento. En el bucle, comprobamos la pendiente actual
+   --con la anterior. Si es superior, los valores actuales pasan a los anteriores, y los anteriores
+   --al auxiliar. De tal forma que cuando lleguemos a una pendiente menor a la anterior,
+   --podamos tener los puntos que han sido los maximos. T1 será el aux y T2 el anterior.
    temp_anterior := temp_actual;
    while (not fin) loop
       Leer(temp_actual);
@@ -35,10 +52,12 @@ begin
       dif_temp := temp_actual - temp_anterior;
       dif_tiempo := tiempo_actual - tiempo_anterior;
 
-      pendiente_actual := dif_temp/dif_tiempo;
+      pendiente_actual := dif_temp/dif_tiempo;           --Aqui el jaleo de los cast para poder dividir bien
       if (pendiente_actual < pendiente_anterior) then
          fin := true;
       else
+         pendiente_anterior := pendiente_actual;
+
          temp_aux := temp_anterior;
          tiempo_aux := tiempo_anterior;
 
@@ -48,11 +67,19 @@ begin
    end loop;
 
 
-   --La temperatura con pendiente maxima es la que se encuentra entre las temperaturas que nos han dado
-   --una pendiente maxima. Por ello, hacemos la media entre estas dos temperaturas.
-   temp_media := ((temp_anterior - temp_aux)/2) + temp_aux      --Offset = temp_aux
 
-   --Calculamos Ct
-   Ct = ((potencia - (Cp*(temp_media - temp_ambiente))) * (tiempo_actual - tiempo_anterior)) / (temp_anterior - temp_aux);
+   --La temperatura con pendiente maxima es la que se encuentra entre las temperaturas que nos han dado
+   --una pendiente maxima. Por ello, hacemos la media entre estas dos temperaturas. Se calcula el valor
+   --intermedio y se le suma el offset, que es la temperatura menor.
+   temp_media := ((temp_anterior - temp_aux)/2) + temp_aux;
+
+   --Calculamos Ct con los datos anteriores.
+   Ct = ((potencia - (Cp * (temp_media - Te))) * (tiempo_actual - tiempo_anterior)) / (temp_anterior - temp_aux);
+
+
+
+   --Apagamos el horno.
+   potencia := 0.0;
+   Escribir(potencia);
 
 end medir1;
